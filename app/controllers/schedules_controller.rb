01a0324daf3643ffbb02cc792fc96cf4c15099c4
kondo0602 <kondo8363@gmail.com>
2021-02-24 22:29:34 +0900

    コントローラからビューにスケジュールの全量ではなく、表示する必要があるスケジュールだけを渡すように変更

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,14 +1,17 @@
 class SchedulesController < ApplicationController
-  before_action :set_all_schedules_instance, only: %i[index create]
-  before_action :set_params_schedules_instance, only: %i[edit update]
-  before_action :set_all_minutes_instance, only: %i[index create edit update destroy]
+  before_action :set_all_minutes_instance
 
   def index
     @schedule = Schedule.new
+    # 一覧に表示する予定だけをビューに渡す
+    start_date = params.fetch(:start_date, Date.today).to_date
+    @schedules = Schedule.where(schedule_date: start_date.beginning_of_week..start_date.end_of_week)
   end
 
   def create
     @schedule = Schedule.new(schedule_params)
+    start_date = params.fetch(:start_date, Date.today).to_date
+    @schedules = Schedule.where(schedule_date: start_date.beginning_of_week..start_date.end_of_week)
     if @schedule.save
       redirect_to root_url
     else
@@ -16,9 +19,12 @@ class SchedulesController < ApplicationController
     end
   end
 
-  def edit; end
+  def edit
+    @schedule = Schedule.find(params[:id])
+  end
 
   def update
+    @schedule = Schedule.find(params[:id])
     if @schedule.update(schedule_params)
       redirect_to root_url
     else
@@ -27,6 +33,7 @@ class SchedulesController < ApplicationController
   end
 
   def destroy
+    @schedule = Schedule.find(params[:id])
     @schedule.destroy
     redirect_to root_url
   end
@@ -39,14 +46,6 @@ class SchedulesController < ApplicationController
                                      :end_time_id,
                                      :title,
                                      :color_id)
-  end
-
-  def set_all_schedules_instance
-    @schedules = Schedule.all
-  end
-
-  def set_params_schedules_instance
-    @schedule = Schedule.find(params[:id])
   end
 
   def set_all_minutes_instance
